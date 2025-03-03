83914a1da8316f6234f52a419f9acaac0d792eda
Julimorg <kienphongtran2003@gmail.com>
2025-03-03 22:24:50 +0700

    Done click out to delete Model for Header

--- src/pages/Home-Page/Home.tsx
+++ src/pages/Home-Page/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './home.module.css'
 import BlurText from '../../components/BlurText/BlurText';
@@ -9,7 +9,58 @@ import { FaBell, FaPlus } from 'react-icons/fa';
 import ButtonIconRight from '../../components/ButtonIconRight/ButtonIconRight';
 
 const fakeAvatar: string = "https://i.pinimg.com/564x/eb/5f/b9/eb5fb972ef581dc0e303b9f80d10d582.jpg";
+// ? API HANDLER LOGIC ONLY
+
+
+// ? VIEW ONLY 
+function NotiModel({ isOpen }: { isOpen: boolean }) {
+    return (
+        <>
+            <div className={isOpen ? `${styles.notiModel} ${styles.show}` : `${styles.notiModel} ${styles.hide}`}>
+
+            </div>
+        </>
+    );
+}
+function UserModel({ isOpen }: { isOpen: boolean }) {
+    return (
+        <>
+            <div className={isOpen ? `${styles.userModel} ${styles.show}` : `${styles.userModel} ${styles.hide}`}>
+                <div className="userModelContent">
+                    <div className="userProfile">
+                        <Link to="/user-profile">User Profile</Link>
+                    </div>
+                    <hr className='hrSpan'/>
+
+                </div>
+            </div>
+        </>
+    )
+}
 const Home = () => {
+    const [isNotiModelOpen, setIsNotiOpen] = useState(false);
+    const [isUserModel, setIstUserModelOpen] = useState(false);
+
+    const notiRef = useRef<HTMLDivElement>(null);
+    const userRef = useRef<HTMLDivElement>(null);
+
+    //? Handle click outside for model
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (notiRef.current && !notiRef.current.contains(event.target as Node)) {
+                setIsNotiOpen(false);
+                console.log(notiRef.current)
+            }
+            if (userRef.current && !userRef.current.contains(event.target as Node)) {
+                setIstUserModelOpen(false);
+                console.log(userRef.current)
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+    }, []);
     return (
         <>
             {/* HEADER */}
@@ -35,16 +86,22 @@ const Home = () => {
                             />
                         </div>
                         {/* Icon Button */}
-                        <div className="notiBtn">
-                            <IconButton Icon={FaBell} size={20} color="#333" onClick={() => alert("Notification clicked!")} />
+                        <div className={styles.notificationContainer} ref={notiRef}>
+                            <IconButton
+                                Icon={FaBell}
+                                size={20}
+                                color="#333"
+                                onClick={() => setIsNotiOpen(!isNotiModelOpen)} />
+                            <NotiModel isOpen={isNotiModelOpen} />
                         </div>
-                        <div className="userProfileBtn">
+                        <div className={styles.userProfileContainer} ref={userRef}>
                             {/* User button */}
                             <UserProfileButton
                                 buttonImg={fakeAvatar}
                                 ImgName='#'
-                            //  onClick = {null} // Thêm Sự kiện thì thêm ở đây
+                                onClick={() => setIstUserModelOpen(!isUserModel)}
                             />
+                            <UserModel isOpen={isUserModel} />
                         </div>
                     </div>
                 </div>
@@ -69,7 +126,14 @@ const Home = () => {
             </div>
             {/* FOOTER */}
             <div className={styles.footer}>
-
+                <div className="footerContainer">
+                    <div className="footerContent">
+                        <p className={`${styles.cursor} ${styles.typewriterAnimation}`}>Xin chào, tụi mình là hội người đẹp trai nhất vũ trụ</p>
+                    </div>
+                    <div className={styles.createdBy}>
+                        <p>&copy; <span id="year"></span> Hội người đẹp trai nhất xóm. All rights reserved.</p>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -3,6 +3,7 @@ import Welcome from "../pages/Welcome-Page/Welcome";
 import Login from "../pages/Login-Page/Login";
 import SignUp from "../pages/SignUp-Page/SignUp";
 import Home from "../pages/Home-Page/Home";
+
 function App() {
   return (
     <Routes>
@@ -10,7 +11,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/home" element={<Home />} />
-
+      
     </Routes>
   );
 }
