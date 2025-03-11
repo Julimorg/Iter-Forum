f1eb09048bedec2d70cd645cb08c8afc2db6246f
Julimorg <kienphongtran2003@gmail.com>
2025-03-11 22:47:23 +0700

    config readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const hostNgrokServer = "4df4-2001-ee0-51d3-b570-f0df-4e45-e38e-52a7";
+const hostNgrokServer = "12fc-2001-ee0-51d3-b570-f0df-4e45-e38e-52a7";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
