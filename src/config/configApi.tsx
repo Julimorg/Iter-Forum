9e16ae2acd7bb2b5cb474f28d107ca2367b804e2
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-06-10 11:18:50 +0700

    adding be api public

--- src/config/configApi.tsx
+++ src/config/configApi.tsx
@@ -1,5 +1,5 @@
 
 //? API Login from BE
-export const API_BE = 'http://localhost:3000';
+export const API_BE = 'https://it-er-forum.onrender.com';
 // export const API_BE = 'http://10.11.77.106:3000';
 
