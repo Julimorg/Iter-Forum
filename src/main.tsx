226bd648b529310079944046422fc4a5c2a1166b
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-06-14 12:58:15 +0700

    config react query

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./routes/App";
 import { BrowserRouter } from "react-router-dom";
+import ReactQueryProvider from "./Provider/ReactQueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter> 
+    <ReactQueryProvider>
       <App />
+      </ReactQueryProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-const hostNgrokServer = "6259-2001-ee0-520e-6be0-11a6-3bf9-2e2-1a21";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [`${hostNgrokServer}.ngrok-free.app`],
-  },
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // alias @ trỏ về thư mục src
+    }
+  }
 })
