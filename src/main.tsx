dec7f73502e7539f37f6f810173dc586ed23d3e9
Julimorg <kienphongtran2003@gmail.com>
2025-06-26 22:30:22 +0700

    Adding Analystic

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import App from "./routes/App";
 import { BrowserRouter } from "react-router-dom";
 import ReactQueryProvider from "./Provider/ReactQueryProvider";
 import '../src/index.css';
-
+import { Analytics } from "@vercel/analytics/react"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter> 
-    <ReactQueryProvider>
-      <App />
+    <BrowserRouter>
+      <ReactQueryProvider>
+        <App />
+        <Analytics />
       </ReactQueryProvider>
     </BrowserRouter>
   </React.StrictMode>
