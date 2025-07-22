f429c28fef704719934c467d4095c32bfb2248e2
Julimorg <kienphongtran2003@gmail.com>
2025-07-22 21:04:28 +0700

    adding vercel.json / config vercel insigth speed

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import ReactQueryProvider from "./Provider/ReactQueryProvider";
 import '../src/index.css';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ReactQueryProvider>
         <App />
         <Analytics />
+        <SpeedInsights />
       </ReactQueryProvider>
     </BrowserRouter>
   </React.StrictMode>
